9a0c4e2bbe6676d14e6c1eef52ce1f4e4da2811f
golammoula287 <golammoula287@gmail.com>
2024-01-09 17:06:08 +0600

    second commit

--- AgeVerify.js
+++ AgeVerify.js
@@ -1,19 +1,23 @@
- // Function to handle age verification
- function verifyAge() {
-    // Get the age from the input field
-    var age = parseInt(document.getElementById("ageInput").value);
+let AGE_NUMBER = 18;
+const inputField = document.getElementById("ageInput");
+const mainBanner = document.querySelector(".Bn-section");
+const submitButton = document.querySelector('button');
 
-    // Check if the age is less than 18
-    if (age < 18) {
-        // Hide the banner section
-        $(".Bn-section").hide(); 
-        // Show a message for children
-        alert("You are a child!");
-    } else if (age >= 18 && age <= 100) {
-        // Show a message for adults
-        document.write("You are an adult!");
-    } else {
-        // Handle other cases if needed
-        alert("Please enter a valid age between 1 and 100.");
+const adultCard = document.querySelector(".adult");
+const teenageCard = document.querySelector(".teenage");
+
+submitButton.addEventListener('click', inputHandler);
+
+function inputHandler() {
+    let inputValue = Number(inputField.value);
+    if (inputValue >= AGE_NUMBER) {
+        adultCard.classList.add("active_adult");
+        teenageCard.classList.remove('active_teenage');
+        //mainBanner.classList.add("remove_banner");
+    }
+    if (inputValue < AGE_NUMBER) {
+        teenageCard.classList.add("active_teenage");
+        adultCard.classList.remove("active_adult");
+        //mainBanner.classList.add("remove_banner");
     }
 }
